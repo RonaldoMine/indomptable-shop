408a2200ba209df08fd4b404abcd12c8069ddb5e
7H3XXX <marcantoine826@gmail.com>
2022-11-30 00:47:45 +0100

    Responsive test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,35 @@
 import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Landing.module.css'
+import logo from '../public/assets/images/belefirst_logo.svg'
+import facebook from '../public/assets/images/facebook.svg'
+import instagram from '../public/assets/images/instagram.svg'
+import shirt from '../public/assets/images/tshirtblack.png'
+
 
 export default function Home() {
   return (
-    <div className='w-screen h-screen bg-black overflow-hidden'>
+    <div className={`${styles.wrapper} w-screen h-screen bg-cover bg-center overflow-hidden`}>
       <Head>
         <title>Coming Soon - Indomptable Shop</title>
         <meta name="description" content="Boutique en ligne des indomptables" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className='text-white text-center absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2'>
-          <h1 className="text-5xl lg:text-9xl md:text-7xl sm:text-6xl font-bold mx-auto text-center self-center text-white">
+      <main className='px-7 py-6'>
+        <Image src={logo} alt="BeleFirst" className='mb-3'/>
+        {/* <h1 className='text-6xl font-black mix-blend-overlay'>
           INDOMPTABLE
-        </h1>
-        <p className='mt-3 text-2xl sm:text-3xl'>Coming Soon...</p>
+        </h1> */}
+        <div>
+          <Image src={shirt} alt="Indomptable t-shirt" className='mt-8 sm:mt-0 w-96 sm:w-[26rem] mx-auto'/>
+        </div>
+        <footer className='w-max flex items-center fixed -translate-x-1/2 left-1/2 bottom-16'>
+          <h3 className='text-4xl font-futura'>Follow us</h3>
+          <a href="https://www.facebook.com/belefirst1"><Image src={facebook} alt="Facebook" className='w-12 ml-8' /></a>
+          <a href="https://www.instagram.com/belefirst1"><Image src={instagram} alt="Instagram" className='w-14 ml-2' /></a>
+          
+        </footer>
       </main>
     </div>
   )
