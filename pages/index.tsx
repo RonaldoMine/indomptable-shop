cfcb4782f92f51403df624b33a6c51a75fc3efa5
7H3XXX <marcantoine826@gmail.com>
2022-11-29 11:46:04 +0100

    Fake landing page responsive done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,8 @@
 import Head from 'next/head'
-import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='w-full h-screen bg-black'>
+    <div className='w-screen h-screen bg-black overflow-hidden'>
       <Head>
         <title>Coming Soon - Indomptable Shop</title>
         <meta name="description" content="Boutique en ligne des indomptables" />
@@ -11,10 +10,10 @@ export default function Home() {
       </Head>
 
       <main className='text-white text-center absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2'>
-          <h1 className="text-9xl font-bold mx-auto text-center self-center text-white">
+          <h1 className="text-5xl lg:text-9xl md:text-7xl sm:text-6xl font-bold mx-auto text-center self-center text-white">
           INDOMPTABLE
         </h1>
-        <p className='mt-3 text-3xl'>Coming Soon...</p>
+        <p className='mt-3 text-2xl sm:text-3xl'>Coming Soon...</p>
       </main>
     </div>
   )
