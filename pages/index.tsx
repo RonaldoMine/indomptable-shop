45d28a64a9c225ce37875b8a08a431855a392867
Ronaldo Mine <andremine98@gmail.com>
2022-12-21 22:22:12 +0100

    _

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,29 +4,14 @@ import styles from '../styles/Landing.module.css'
 import logo from '../public/assets/images/logo.svg'
 import shirt_black from '../public/assets/images/tshirt-black-desktop.png'
 import shirt_white from '../public/assets/images/tshirt-white-desktop.png'
-import light_svg from '../public/assets/images/icones/light.svg'
-import dark_svg from '../public/assets/images/icones/dark.svg'
 import {useState} from "react";
 
 
 export default function Home() {
-    const [theme, setTheme] = useState("light");
-    const [theme_svg, setSvgTheme] = useState(light_svg);
     const [colorTShirt, setColorTShirt] = useState('white');
     const [whiteOptionsView, setWhiteOptionView] = useState({opacity: 1, display: "block"});
     const [blackOptionsView, setBlackOptionView] = useState({opacity: 0, display: "none"});
 
-    const handleChangeTheme = () => {
-        if (theme == 'dark') {
-            setTheme("light");
-            setSvgTheme(light_svg);
-            document.documentElement.classList.remove('dark')
-        } else {
-            setTheme("dark");
-            setSvgTheme(dark_svg);
-            document.documentElement.classList.add('dark')
-        }
-    }
     const handleChangeTShirt = (color: string) => {
         if (color === 'white') {
             setWhiteOptionView({display: 'block', opacity: 1})
@@ -54,10 +39,6 @@ export default function Home() {
                         <a href="https://www.facebook.com/belefirst1" className='text-white'>Facebook</a>
                         <a href="https://www.instagram.com/belefirst1" className='text-white ml-8'>Instagram</a>
                     </div>
-                    {/*<button
-                        className="hidden bg-white transition ease-in dark:bg-neutral-900 dark:border-2 font-medium w-12 h-12 flex justify-center items-center"
-                        onClick={handleChangeTheme} style={{borderRadius: "100%"}}>
-                        <Image alt={"Thème"} src={theme_svg} width={24} height={24}/></button>*/}
                 </div>
                 <div className='sm:flex grid justify-center items-center sm:mt-0 mx-auto h-full'>
                     <div className={"grid md:flex relative justify-center items-center sm:w-screen"}>
