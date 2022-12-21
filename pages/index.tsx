be51bad2cfbf6d3410a39359babfb5b6f74f929f
Ronaldo Mine <andremine98@gmail.com>
2022-12-21 23:26:09 +0100

    _

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,7 +31,6 @@ export default function Home() {
                 <meta name="description" content="Boutique en ligne des indomptables"/>
                 <link rel="icon" href="/assets/images/logo.svg"/>
             </Head>
-
             <main className='md:px-32 px-10 py-6 relative z-10'>
                 <div className="grid sm:justify-items-end justify-items-center mx-auto sm:flex">
                     <Image src={logo} alt="BeleFirst" className='mb-6'/>
@@ -42,7 +41,8 @@ export default function Home() {
                 </div>
                 <div className='sm:flex grid justify-center items-center sm:mt-0 mx-auto h-full'>
                     <div className={"grid md:flex relative justify-center items-center sm:w-screen"}>
-                        <div className="md:grid md:mt-0 md:order-1 flex justify-center md:content-center rounded-full mr-5 mb-4 order-2 z-10">
+                        <div
+                            className="md:grid md:mt-0 md:order-1 flex justify-center md:content-center rounded-full mr-5 mb-4 order-2 z-10">
                             <button onClick={() => handleChangeTShirt('white')}
                                     className={`w-10 h-10 rounded-full flex justify-center items-center ml-2 mr-2 ${colorTShirt === 'white' && styles.item}`}>
                                 <div className={`bg-white rounded-full w-2 h-2`}></div>
@@ -75,9 +75,9 @@ export default function Home() {
                                 <h1 className='text-8xl font-futura font-bold text-white mb-4 md:text-7xl lg:text-9xl block'>THE <br/> MBOA
                                 </h1>
                                 <h2 className='text-1xl font-space font-bold'>INDOMPTABLE THE WHITE TEE</h2>
-                                <p>Wear proundly our colors, show your identity, <br/> show your worth, show your
-                                    continental
-                                    attitude</p>
+                                <p className={"font-space"}>Show them who you are with this vibrant colorful shirt. <br/>Soft cotton and a light
+                                    fabric keeps you comfortable day and night. <br/> Ethnical graphics walk you through
+                                    beauty, africanity and success.</p>
                             </div>
                             <div
                                 style={{
@@ -87,9 +87,9 @@ export default function Home() {
                                 <h1 className='text-8xl font-futura font-bold text-white mb-4 md:text-7xl lg:text-9xl'>THE <br/> HEMLE
                                 </h1>
                                 <h2 className='text-1xl font-space font-bold'>INDOMPTABLE THE BLACK TEE</h2>
-                                <p>Wear proundly our colors, show your identity, <br/> show your worth, show your
-                                    continental
-                                    attitude</p>
+                                <p className={"font-space"}>We make our dreams come true, we write our story. <br/> HEMLE is the word of those
+                                    who make history. <br/> You can pull on this premium quality cotton tee <br/> with golden
+                                    graphics to remember what you capable of. Be limitless.</p>
                             </div>
                         </div>
                     </div>
