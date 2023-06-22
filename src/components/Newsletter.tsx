d6e4aacf7132237794f5d9c1a47aaced483972c9
7H3XXX <marcantoine826@gmail.com>
2023-06-22 01:53:54 +0100

    Home Card item fixed

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -1,93 +1,102 @@
-import React, {useState} from "react";
-import {useForm} from "react-hook-form";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 import Alert from "./Alert";
-import {useTranslation} from "next-i18next";
+import { useTranslation } from "next-i18next";
 import parallax_photo from "../../public/assets/images/parallax-home.webp";
 
-
 export default function Newsletter() {
-    const {register, reset, handleSubmit} = useForm();
-    const [showAlert, setShowAlert] = useState(false);
-    const [onLoading, setOnLoading] = useState(false);
-    const [configAlert, setConfigAlert] = useState({title: "", text: "", status: ""});
-    const {t, i18n} = useTranslation("newsletter")
+  const { register, reset, handleSubmit } = useForm();
+  const [showAlert, setShowAlert] = useState(false);
+  const [onLoading, setOnLoading] = useState(false);
+  const [configAlert, setConfigAlert] = useState({
+    title: "",
+    text: "",
+    status: "",
+  });
+  const { t, i18n } = useTranslation("newsletter");
 
-    const handleSubmitNewsletterForm = async (data: any) => {
-        setOnLoading(true);
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                ...data,
-                lang: i18n.language
-            }),
-        }
-        const response = await fetch("/api/newsletter", options);
-        const result = await response.json();
-        const status = response.status
-        if (status == 200) {
-            reset();
-            setConfigAlert({title: "Success", text: result.message, status: "success"})
-        } else {
-            setConfigAlert({title: "Erreur", text: result.message, status: status === 400 ? "warning" : "danger"})
-        }
-        setShowAlert(true);
-        setOnLoading(false);
+  const handleSubmitNewsletterForm = async (data: any) => {
+    setOnLoading(true);
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        ...data,
+        lang: i18n.language,
+      }),
+    };
+    const response = await fetch("/api/newsletter", options);
+    const result = await response.json();
+    const status = response.status;
+    if (status == 200) {
+      reset();
+      setConfigAlert({
+        title: "Success",
+        text: result.message,
+        status: "success",
+      });
+    } else {
+      setConfigAlert({
+        title: "Erreur",
+        text: result.message,
+        status: status === 400 ? "warning" : "danger",
+      });
     }
+    setShowAlert(true);
+    setOnLoading(false);
+  };
 
-    return (
-        <>
-            <div
-                id="newsletter"
-                className="dark:bg-neutral-800 bg-fixed bg-center sm:bg-cover"
-                style={{backgroundImage: `url('${parallax_photo.src}')`}}
-            >
-                <div className={"lg:w-4/6 md:w-5/6 w-full p-4 mx-auto"}>
-                    <h1 className={"text-center text-4xl md:text-6xl font-bold mb-2 text-gradient-simple"}>
-                        {t("title")}
-                    </h1>
-                    <p className={"text-center text-white mb-16"}>
-                        {t("subtitle")}
-                    </p>
-                    <form
-                        onSubmit={handleSubmit(handleSubmitNewsletterForm)}
-                        method={"post"}
-                        className={'flex items-center max-w-[500px] sm:w-3/4 w-ful mx-auto'}
-                    >
-                        <input
-                            type="email"
-                            className={
-                                "ring:border-red-500 text-neutral-800 mb-0 bg-white "
-                            }
-                            placeholder={`${t("form.email")}`}
-                            {...register("email", {
-                                required: true,
-                                disabled: onLoading
-                            })}
-                        />
-                        {onLoading ? (
-                            <span className="loader ml-4"></span>
-                        ) : (
-                            <button
-                                className={
-                                    "hover:bg-opacity-20 text-white bg-gradient"
-                                }
-                            >
-                                {t("form.button")}
-                            </button>
-                        )}
-                    </form>
-                </div>
-            </div>
-            <Alert
-                type={configAlert.status}
-                visible={showAlert}
-                setVisible={setShowAlert}
-                title={configAlert.title}
-                text={configAlert.text}
+  return (
+    <>
+      <div
+        id="newsletter"
+        className="dark:bg-neutral-800 bg-fixed bg-center sm:bg-cover"
+        style={{ backgroundImage: `url('${parallax_photo.src}')` }}
+      >
+        <div className={"lg:w-4/6 md:w-5/6 w-full p-4 mx-auto"}>
+          <h1
+            className={
+              "text-center text-4xl md:text-6xl font-bold mb-2 text-gradient-simple"
+            }
+          >
+            {t("title")}
+          </h1>
+          <p className={"text-center text-white mb-16"}>{t("subtitle")}</p>
+          <form
+            onSubmit={handleSubmit(handleSubmitNewsletterForm)}
+            method={"post"}
+            className={"flex items-center max-w-[500px] sm:w-3/4 w-ful mx-auto"}
+          >
+            <input
+              type="email"
+              className={
+                "ring:border-red-500 text-neutral-800 bg-neutral-50 focus:bg-white "
+              }
+              placeholder={`${t("form.email")}`}
+              {...register("email", {
+                required: true,
+                disabled: onLoading,
+              })}
             />
-        </>
-    );
+            {onLoading ? (
+              <span className="loader ml-4"></span>
+            ) : (
+              <button className={"hover:bg-opacity-20 text-white bg-gradient"}>
+                {t("form.button")}
+              </button>
+            )}
+          </form>
+        </div>
+      </div>
+      <Alert
+        type={configAlert.status}
+        visible={showAlert}
+        setVisible={setShowAlert}
+        title={configAlert.title}
+        text={configAlert.text}
+      />
+    </>
+  );
 }
